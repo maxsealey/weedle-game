2d2c0342af9df0c167bebfdc7c5901b325b09c7e
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-20 00:30:36 -0700

    created components for dropdown menu and results display grid, Pokemon class

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Grid extends Component {
+export default class Dropdown extends Component {
   render() {
     return (
       <div></div>
---
+++ src/components/Pokemon.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Pokemon extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount(){
+        
+    }
+
+    render() {
+        return (
+            <div>Pokemon</div>
+        )
+    }
+}
