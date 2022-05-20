b94f01d0adc3c3f7f84eca3e1679493073978952
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-20 00:27:22 -0700

    imported layout components and bootstrap link, css edits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import './App.css'
+import Header from './components/layout/Header'
+import Dashboard from './components/layout/Dashboard'
+import Footer from './components/layout/Footer'
 
 export default class App extends Component {
   render() {
     return (
-      <div></div>
+      <div className="App">
+        <Header/>
+        <Dashboard/>
+        <Footer/>
+      </div>
     )
   }
 }
