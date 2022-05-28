8043a0eff726aaf4dda137a2ee3fc920c4fef744
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-28 01:17:03 -0700

    axios retrieves data from pokeapi

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import Dropdown from '../Dropdown'
+import axios from 'axios'
 import '../../App.css'
 
 export default class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        url: "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0",
+        pokemon: null // stores retrieved data
+    }
+  }
+
+  async componentDidMount() {
+      const res = await axios.get(this.state.url)
+      this.setState({ pokemon: res.data['results'] })
+  } // asynchronously gets data from pokeapi
 
   render() {
     const titleStyle = {color:'#ffcb05', paddingTop:'10px', fontSize:'70px', fontFamily:'Pokemon Solid', letterSpacing:'4px', textShadow:"3px 0 0 #3466af, -3px 0 0 #3466af, 0 3px 0 #3466af, 0 -3px 0 #3466af, 2px 2px #3466af, -2px -2px 0 #3466af, 2px -2px 0 #3466af, -2px 2px 0 #3466af"}
