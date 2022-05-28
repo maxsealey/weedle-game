e09cb60da3180704612ec45f66e09139805aa93f
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-27 21:39:10 -0700

    imported mui elements for drop down menu, created pokemans array of objects for testing

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react'
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
-export default class Dropdown extends Component {
-  render() {
-    return (
-      <div></div>
-    )
-  }
+export default function Dropdown() {
+  return (
+    <Autocomplete
+      disablePortal
+      id="pokemon-list"
+      options={pokemans}
+      sx={{ width: 350 }}
+      renderInput={(params) => <TextField {...params} label="Choose a Pokemon"/>}
+    />
+  );
 }
+
+const pokemans = [
+  {
+    label:"Bulbasaur",
+    dexNum:1,
+    gen:1,
+    type:["grass","poison"],
+    evoStage:1,
+    eggGroup:["monster","grass"]
+  },
+  {
+    label:"Ivysaur",
+    dexNum:2,
+    gen:1,
+    type:["grass","poison"],
+    evoStage:2,
+    eggGroup:["monster","grass"]
+  },
+  {
+    label:"Venusaur",
+    dexNum:3,
+    gen:1,
+    type:["grass","poison"],
+    evoStage:3,
+    eggGroup:["monster","grass"]
+  }
+]
