47dfa6937064edfc8f5a1c2bdc7a9e25b3be68de
Max Sealey <maxsealey@users.noreply.github.com>
2022-07-11 14:41:43 -0700

    share and settings visible for testing purposes

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,1 +1,10 @@
 import '../style/settings.scss'
+import React, { Component } from 'react'
+
+export default class Settings extends Component {
+  render() {
+    return (
+      <div className='settings'></div>
+    )
+  }
+}
--- src/components/Share.js
+++ src/components/Share.js
@@ -1,1 +1,11 @@
 import '../style/share.scss'
+
+import React, { Component } from 'react'
+
+export default class Share extends Component {
+  render() {
+    return (
+      <div className="share">Share</div>
+    )
+  }
+}
