8f847c526f4f483e6fdf8656895e514f1fbf9176
Max Sealey <maxsealey@users.noreply.github.com>
2024-10-19 19:28:29 -0700

    stash

--- server/models/pokemon.go
+++ server/models/pokemon.go
@@ -1,1 +1,19 @@
 package models
+
+type Pokemon struct {
+	Name     string  `json:"name"`
+	DexNum   int     `json:"id"`
+	Gen      int     `json:"generation"`
+	Types    []Type  `json:"types"`
+	Height   float32 `json:"height"`
+	EvoStage int     `json:""`
+}
+
+type Types struct {
+	Slot int      `json:"slot"`
+	Type TypeInfo `json:"type"`
+}
+
+type TypeInfo struct {
+	Name string `json:"name"`
+}
