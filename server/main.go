cbd310923ff62b22ef2aff1400b18d8979068602
Max Sealey <maxsealey@users.noreply.github.com>
2024-10-26 15:48:43 -0700

    Backend external API call functionality completed

--- server/main.go
+++ server/main.go
@@ -2,50 +2,19 @@ package main
 
 import (
 	"fmt"
-	"server/models"
 	"server/services"
-	"sync"
 )
 
 func main() {
-	dex := 1
+	//var pokemon models.Pokemon
 
-	basicInfoCh := make(chan models.PokemonBasicInfo)
-	speciesInfoCh := make(chan models.PokemonSpeciesInfo)
+	pokemon := services.ExecuteConcurrentAPICalls()
 
-	var wg sync.WaitGroup
-	wg.Add(2)
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Dex Number: %d\n", pokemon.DexNum)
+	fmt.Printf("Generation: %d\n", pokemon.Generation)
+	fmt.Printf("Types: %s, %s\n", pokemon.Types[0].Type.Name, pokemon.Types[1].Type.Name)
+	fmt.Printf("Egg Groups: %s, %s\n", pokemon.EggGroups[0].Name, pokemon.EggGroups[1].Name)
+	fmt.Printf("Color: %s\n", pokemon.Color)
 
-	go services.FetchBasicInfo(dex, basicInfoCh, &wg)
-	go services.FetchSpeciesInfo(dex, speciesInfoCh, &wg)
-
-	wg.Wait()
-
-	var basicInfo models.PokemonBasicInfo
-	var speciesInfo models.PokemonSpeciesInfo
-
-	select {
-	case basicInfo = <-basicInfoCh:
-		// Successfully received basic info
-		fmt.Println("Basic info received")
-	default:
-		fmt.Println("No basic info received")
-	}
-
-	select {
-	case speciesInfo = <-speciesInfoCh:
-		// Successfully received species info
-		fmt.Println("Species info received")
-	default:
-		fmt.Println("No species info received")
-	}
-
-	completePokemon := services.CompilePokemonInfo(basicInfo, speciesInfo)
-	fmt.Printf("Dex Number: %d\n", completePokemon.DexNum)
-	fmt.Printf("Name: %s\n", completePokemon.Name)
-	fmt.Printf("Color: %s\n", speciesInfo.Color)
-
-	// Close channels after goroutines finish
-	close(basicInfoCh)
-	close(speciesInfoCh)
 }
