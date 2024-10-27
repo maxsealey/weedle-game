920852b18982c1c48d4f6cd869df134d8120cc40
Max Sealey <maxsealey@users.noreply.github.com>
2024-10-26 19:23:06 -0700

    created handler to convert names to json

--- server/handlers/pokemon.go
+++ server/handlers/pokemon.go
@@ -1,1 +1,20 @@
 package handlers
+
+import (
+	"net/http"
+	"server/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Handler to get Pokémon names and ids
+func GetPokemonNames(c *gin.Context) {
+	pokemonList := services.FetchNameList()
+
+	if pokemonList == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch Pokémon names"})
+		return
+	}
+
+	c.JSON(http.StatusOK, pokemonList)
+}
