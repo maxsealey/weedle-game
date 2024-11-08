abb4880b5a34c48e3da415f03aa1219c45d3d77f
Max Sealey <maxsealey@users.noreply.github.com>
2024-11-07 21:58:31 -0800

    Created class for Pokemon objects

---
+++ client/src/models/Pokemon.js
@@ -0,0 +1,10 @@
+export default class Pokemon {
+    constructor(name, dex, gen, types, eggGroups, color){
+        this.name = name
+        this.dex = dex
+        this.gen = gen
+        this.types = types
+        this.eggGroups = eggGroups
+        this.color = color
+    }
+}
