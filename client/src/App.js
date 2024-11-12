97b8aeb75553056bddd02f72ca3eab5972826457
Max Sealey <maxsealey@users.noreply.github.com>
2024-11-11 19:53:00 -0800

    Removed Footer; Dropdown selections populate grid rows (test data)

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import React from 'react'
 import './index.scss'
 import Header from './components/layout/Header/Header'
 import Dashboard from './components/layout/Dashboard/Dashboard'
-import Footer from './components/layout/Footer/Footer'
 
 const App = () => {
     return (
       <div className="App">
         <Header/>
         <Dashboard />
-        <Footer/>
       </div>
     )
 }
